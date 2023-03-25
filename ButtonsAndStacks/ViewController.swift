cea3e1f4268820266ad4e937909d8419129a485d
YHYen <cloudyhyen@gmail.com>
2023-03-26 01:03:15 +0800

    Section 2.12 label change color

--- ButtonsAndStacks/ViewController.swift
+++ ButtonsAndStacks/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorLabel: UILabel!
+    var colors: [UIColor] = [.red, .orange, .yellow, .green, .blue, .systemIndigo, .systemPurple]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func colorButtonPressed(_ sender: UIButton) {
+        print("You clicked tag #: \(sender.tag)")
+        // print(sender.currentTitle!)
+        
+        if sender.tag >= 0 && sender.tag <= 7 {
+            colorLabel.text = "You clicked \(sender.currentTitle!)"
+            colorLabel.textColor = colors[sender.tag]
+        } else if sender.tag == 1000 {
+            colorLabel.text = ""
+        }
+        
+    }
 }
 
